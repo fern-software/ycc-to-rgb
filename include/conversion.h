599a11f4faf9cd2259fee98b61c97cf5cf0bc8fd
RcrossF <finnmorin@gmail.com>
2022-07-02 17:08:59 -0700

    Add code to compute ycc to rgb using floating point math

--- include/conversion.h
+++ include/conversion.h
@@ -1,3 +1,8 @@
 #pragma once
 
-void ycc_to_rgb(struct ycc_img* src_img, struct rgb_img* dst_img);
+#include "util.h"
+
+
+void single_value_ycc_to_rgb(struct ycc_pixel *pixel, struct rgb_pixel *rgb_pixel);
+
+void ycc_to_rgb(struct ycc_img *src_img, struct rgb_img *dst_img);
