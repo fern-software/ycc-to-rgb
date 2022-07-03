fe04ff4ea40906b47006d1f5cccb3ff3d4ae01d2
RcrossF <finnmorin@gmail.com>
2022-07-02 17:10:21 -0700

    Add function header comments

--- include/conversion.h
+++ include/conversion.h
@@ -2,7 +2,9 @@
 
 #include "util.h"
 
-
+// Converts a single YCC pixel to an RGB pixel.
 void single_value_ycc_to_rgb(struct ycc_pixel *pixel, struct rgb_pixel *rgb_pixel);
 
+// Converts a YCC image to an RGB image.
+// Note: allocates (width*height*sizeof(rgb_pixel)) memory for dst_img. This can most likely be optimized.
 void ycc_to_rgb(struct ycc_img *src_img, struct rgb_img *dst_img);
